15a0434b6ff27a79c1a4024dafa0b8a0671e2d7e
Timothy Maloney <tmaloney@pdx.edu>
2021-10-13 14:06:01 -0700

    Added pretty output for catch

--- src/catch/mod.rs
+++ src/catch/mod.rs
@@ -55,7 +55,25 @@ pub fn run(options: &Options) {
         Protocol::Udp => {
             let mut udp_metadata = udp::Metadata::new(&options.local[..]);
             set_amount_and_time_udp(options, &mut udp_metadata);
-            udp_metadata.collect();
+
+            let now = std::time::Instant::now();
+            let datagrams = udp_metadata.collect();
+            let time = now.elapsed().as_secs();
+
+            let mut total_bytes = 0;
+            for datagram in &datagrams {
+                // length of payload + length of header
+                total_bytes += datagram.0.data.len() as u32 + 8;
+            }
+
+            let received = datagrams.len() as f32 / options.amount.unwrap() as f32;
+
+            print_total_stats(time, total_bytes, received);
+
+            print_datagram_stats_header();
+            for datagram in &datagrams {
+                print_udp(&datagram);
+            }
         }
         Protocol::Tcp => {
             println!("tcp")
@@ -72,6 +90,50 @@ fn set_amount_and_time_udp(options: &Options, udp_metadata: &mut udp::Metadata) 
     }
 }
 
-fn set_amount_and_time_tcp(options: &Options) {
+fn set_amount_and_time_tcp(_options: &Options) {
+    todo!()
+}
+
+fn print_total_stats(total_time: u64, total_bytes: u32, received: f32) {
+    println!();
+    println!(
+        "{time:<width$}{bytes:<width$}{received:<width$}",
+        time = "time",
+        bytes = "bytes",
+        received = "received",
+        width = 10
+    );
+    println!("{line:=<width$}", line = "", width = 28);
+
+    let mut received = received.to_string();
+    received.push_str("%");
+
+    let mut total_time = total_time.to_string();
+    total_time.push_str("s");
+    println!(
+        "{time:<width$}{bytes:<width$}{received:<width$}",
+        time = total_time,
+        bytes = total_bytes,
+        received = received,
+        width = 10
+    );
+}
+
+fn print_datagram_stats_header() {
+    println!();
+    println!(
+        "{src:<width$}{dst:<width$}{len:<width$}{chksm:<width$}{time:<width$}{bytes:<width$}",
+        src = "src",
+        dst = "dst",
+        len = "len",
+        chksm = "chksm",
+        time = "time",
+        bytes = "bytes",
+        width = 12
+    );
+    println!("{line:=<width$}", line = "", width = 65);
+}
+
+fn print_udp(_datagram: &(udp::UdpDatagram, u32)) {
     todo!()
 }
