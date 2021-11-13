e563842ebabc68f7441ac3235e719b9bd7768e10
Timothy Maloney <tmaloney@pdx.edu>
2021-11-13 12:12:51 -0800

    Chore: moved Protocol enum to tacklebox crate

--- src/catch/mod.rs
+++ src/catch/mod.rs
@@ -1,8 +1,8 @@
 extern crate structopt;
 use structopt::StructOpt;
 use tacklebox::tcp::TcpMetadata;
-use tacklebox::udp::{UdpDatagram, UdpMetadata};
-use thiserror::Error;
+use tacklebox::udp::UdpMetadata;
+use tacklebox::Protocol;
 
 use std::io::Error as IoErr;
 use std::io::ErrorKind as IoErrKind;
@@ -13,72 +13,33 @@ pub struct Options {
     pub protocol: Protocol,
     #[structopt(help = "Socket to send datagrams from")]
     pub local: String,
-    #[structopt(short, help = "Verbose output")]
-    pub verbose: bool,
     #[structopt(short = "n", help = "Amount of datagrams to collect")]
     pub amount: Option<u16>,
     #[structopt(short, long, help = "Time to spend waiting for individual datagrams")]
     pub time: Option<u32>,
 }
 
-#[derive(Error, Debug)]
-pub enum ParseErr {
-    #[error("Invalid protocol")]
-    Protocol,
-}
-
-#[derive(Debug)]
-pub enum Protocol {
-    Udp,
-    Tcp,
-}
-
-use std::str::{self, FromStr};
-impl FromStr for Protocol {
-    type Err = ParseErr;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "udp" => Ok(Protocol::Udp),
-            "tcp" => Ok(Protocol::Tcp),
-            _ => Err(ParseErr::Protocol),
-        }
-    }
-}
-
-use std::fmt;
-impl fmt::Display for Protocol {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", &self.to_string()[..])
-    }
-}
-
 pub fn run(options: &Options) {
+    let (_amount, wait_time) = set_amount_and_time(&options);
     match options.protocol {
         Protocol::Udp => {
             let udp_metadata = UdpMetadata::new(&options.local[..]);
-            let (amount, wait_time) = set_amount_and_time(&options);
 
             let now = std::time::Instant::now();
-            let datagrams = udp_metadata.collect_datagrams(amount, wait_time).unwrap();
+            let datagrams = udp_metadata.collect_datagrams(_amount, wait_time).unwrap();
             let time = now.elapsed().as_secs();
 
             let mut total_bytes = 0;
             for datagram in &datagrams {
-                // length of payload + length of header
-                total_bytes += datagram.0.data.len() as u32 + 8;
+                total_bytes += datagram.0.data.len() as u32;
             }
 
-            let received = datagrams.len() as f32 / amount as f32;
+            let received = datagrams.len() as f32 / _amount as f32;
 
-            if !options.verbose {
-                print_total_stats(time, total_bytes, received);
-            } else {
-                print_datagram_stats(datagrams);
-            }
+            print_total_stats(time, total_bytes, received);
         }
         Protocol::Tcp => {
             let mut tcp_metadata = TcpMetadata::new(&options.local[..]);
-            let (_amount, wait_time) = set_amount_and_time(&options);
 
             let _block = IoErr::from(IoErrKind::WouldBlock);
 
@@ -86,11 +47,7 @@ pub fn run(options: &Options) {
             if let Err(_block) = tcp_metadata.wait_for_connection(wait_time) {}
             let time = now.elapsed().as_secs();
 
-            if !options.verbose {
-                print_total_stats(time, 0, 0.0);
-            } else {
-                println!("verbose!");
-            }
+            print_total_stats(time, 0, 0.0);
         }
     }
 }
@@ -139,29 +96,4 @@ fn print_total_stats(total_time: u64, total_bytes: u32, received: f32) {
     );
 
     println!();
-}
-
-fn print_datagram_stats(datagrams: Vec<(UdpDatagram, u32)>) {
-    println!();
-
-    println!(
-        "{src:<width$}{dst:<width$}{len:<width$}{chksm:<width$}{time:<width$}{bytes:<width$}",
-        src = "src",
-        dst = "dst",
-        len = "len",
-        chksm = "chksm",
-        time = "time",
-        bytes = "bytes",
-        width = 12
-    );
-
-    println!("{line:=<width$}", line = "", width = 65);
-
-    for datagram in &datagrams {
-        print_udp(&datagram);
-    }
-}
-
-fn print_udp(_datagram: &(UdpDatagram, u32)) {
-    todo!()
 }
--- tacklebox/src/lib.rs
+++ tacklebox/src/lib.rs
@@ -1,2 +1,35 @@
+use thiserror::Error;
+use std::fmt;
+use std::str::{self, FromStr};
+
 pub mod tcp;
 pub mod udp;
+
+#[derive(Error, Debug)]
+pub enum ParseErr {
+    #[error("Invalid protocol")]
+    Protocol,
+}
+
+#[derive(Debug)]
+pub enum Protocol {
+    Udp,
+    Tcp,
+}
+
+impl FromStr for Protocol {
+    type Err = ParseErr;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "udp" => Ok(Protocol::Udp),
+            "tcp" => Ok(Protocol::Tcp),
+            _ => Err(ParseErr::Protocol),
+        }
+    }
+}
+
+impl fmt::Display for Protocol {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", &self.to_string()[..])
+    }
+}
